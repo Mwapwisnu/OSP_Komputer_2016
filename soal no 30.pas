34f8fde6f6f4f966cb1f82a14116726edf5e14f2
Mwapwisnu <30758731+Mwapwisnu@users.noreply.github.com>
2017-08-14 07:45:26 +0700

    this new character and repair for output
    Sudah saya perbaiki.
     sebelumnya salah pada penjumlahan x + y ;

--- soal no 30.pas
+++ soal no 30.pas
@@ -2,19 +2,22 @@ Program OSP_16_30;
 Uses crt;
   var
   x,y: integer;
+  xx,yy : integer;
  begin
    x := 1;
    y := 0;
-   while(x <= 10) do
-   begin
-     y := y + x;
-     writeln('penjumlahan ',y,' + ',x,' =',y);
-     x := x + x;
-     writeln('penjumlahan ',x,' + ',y,' =',x);
-   end;
- writeln('y:= ',y,' + ',x,' = ',y);
- writeln;
- writeln(y);
- readln();
+     while(x <= 10) do
+       begin
+         y := y + x;
+         x := x + x;
+         xx := x + x;
+         yy := y + x;
+         writeln('penjumlahan y(',y,') + x(',x,') =',yy);
+         writeln('penjumlahan x(',x,') + x(',x,') =',xx);
+       end;
+   writeln;
+   writeln(y);
+   readln();
  end.
+
 
