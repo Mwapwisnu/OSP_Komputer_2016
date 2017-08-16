60c8fc090cc582ee9b7e13220d1b7ff91da3fc1b
Mwapwisnu <30758731+Mwapwisnu@users.noreply.github.com>
2017-08-16 07:09:44 +0700

    untuk mengeluarkan perhitungannya

--- Soal no 41.pas
+++ Soal no 41.pas
@@ -1,28 +1,39 @@
-Var 
-   Q : array[1..15] of integer; // = (1,5,2,3,4,6,2,7,3,4,6,0,-1,2,3);
-   P : array[1..15] of integer;
-i,a  : integer;
-b,total : integer;
-  begin
+Program Osp_16_41;
+uses crt;
+Var
+Q : array[1..15] of integer ;
+P : array[0..15] of integer;
+i, a, b,total,r : integer;
+begin
+  Q[1]:=1;        Q[9]:=3;
+  Q[2]:=5;        Q[10]:=4;
+  Q[3]:=2;        Q[11]:=6;
+  Q[4]:=3;        Q[12]:=0;
+  Q[5]:=4;        Q[13]:=-1;
+  Q[6]:=6;        Q[14]:=2;
+  Q[7]:=2;        Q[15]:=3;
+  Q[8]:=7;
 
-  Q[1]:= 1;         Q[9] := 3;
-  Q[2]:= 5;         Q[10]:= 4;
-  Q[3]:= 2;         Q[11]:= 6;
-  Q[4]:= 3;         Q[12]:= 0;
-  Q[5]:= 4;         Q[13]:= -1;
-  Q[6]:= 6;         Q[14]:= -2;
-  Q[7]:= 2;         Q[15]:= 3;
-  Q[8]:= 7;
-
-       for i :=  1 to 15 do
-         begin
-		   P[i] := P[i-1] + Q;
-         end;
-     readln(a,b);
-     total := 0;			//baris -1
-       for i := a to b do		//baris -2
-          begin
-            total := total + Q[i];	//baris -3
-          end;			//baris -4
-     writeln(total);
-  end.
+       //readln (a,b);
+       a := 6;
+       b := 10;
+       writeln;
+       writeln('penjumlahan array Q dari ',a,' ke ',b,'  : ');
+       writeln;
+           total := 0;           // baris-1
+    for i := a to b do    // baris-2
+      begin
+          r := total + Q[i];
+            writeln(Q[i],' + ');
+    writeln('total := ',total,' + Q[',Q[i],'] = ',r);
+           total := total + Q[i]; // baris-3
+      //total := P[b] - P[a-1];
+      end; // baris-4
+  //writeln;
+  writeln;
+  writeln;
+  writeln(total);
+  writeln;
+  writeln('jadi total nya ialah : ',total);
+  readln;
+end.
